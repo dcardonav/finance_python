8add9c91af2905582944eedcf69ffe3fe2c6c841
David Cardona-Vasquez <dcardon@gmail.com>
2021-12-25 14:37:37 +0100

    fin estudio semana 1

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import scipy.stats
+
 
 def drawdown(return_series: pd.Series):
     """
@@ -12,12 +14,12 @@ def drawdown(return_series: pd.Series):
     :return:
     """
 
-    wealth_index = 1000*(1+return_series).cumprod()
+    wealth_index = 1000 * (1 + return_series).cumprod()
     previous_peaks = wealth_index.cummax()
-    drawdowns = (wealth_index -previous_peaks)/previous_peaks
+    drawdowns = (wealth_index - previous_peaks) / previous_peaks
 
     return pd.DataFrame({
-        'wealth':wealth_index,
+        'wealth': wealth_index,
         'peaks': previous_peaks,
         'drawdown': drawdowns
     })
@@ -32,7 +34,115 @@ def get_ffme_returns():
                        header=0, index_col=0, na_values=-99.99)
     rets = me_m[['Lo 10', 'Hi 10']]
     rets.columns = ['SmallCap', 'LargeCap']
-    rets = rets/100
+    rets = rets / 100
     rets.index = pd.to_datetime(rets.index, format='%Y%m').to_period('M')
 
     return rets
+
+
+def get_hfi_returns():
+    """
+    Load and format the EDHEC Hedge Fund Index Returns
+    """
+
+    hfi = pd.read_csv("course_1/edhec-hedgefundindices.csv",
+                      header=0, index_col=0, parse_dates=True)
+    hfi = hfi / 100
+    hfi.index = hfi.index.to_period('M')
+
+    return hfi
+
+
+def skewness(r):
+    """
+    Alternative to scipy.stats.skew()
+    Computes the skewness of the supplied Series or DataFrame
+    Returns a float or a Series
+    """
+
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**3).mean()
+    return exp/sigma_r**3
+
+
+
+
+def kurtosis(r):
+    """
+    Alternative to scipy.stats.kurtosis()
+    Computes the kurtosis of the supplied Series or DataFrame
+    Returns a float or a Series
+    """
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**4).mean()
+    return exp/sigma_r**4
+
+
+def is_normal(r, level=0.01):
+    """
+    Applies the Jarque-Bera test to determine if a Series is normal or not
+    Test is applied at the 1% level by default
+    Returns True if the hypothesis of normality is not rejected, False otherwise
+    """
+    statistic, p_value = scipy.stats.jarque_bera(r)
+
+    return p_value > level
+
+def semideviation(r):
+    """
+    Returns the semi-deviation (a.k.a.: negative semideviation of r
+    r must be a Series or DataFrame
+    """
+
+    is_negative = r < 0
+    return r[is_negative].std(ddof=0)
+
+def var_historic(r, level=5):
+    """
+    Returns the historic Value at Risk at a specified level
+    i.e. returns the number such that "level" percent of the returns
+    fall below that number, and the (100-level) percent are above
+    """
+    if isinstance(r, pd.DataFrame):
+        return r.aggregate(var_historic, level=level) # calls the function again, but this time over each column
+    elif isinstance(r, pd.Series):
+        return -np.percentile(r, level) # minus for reporting purposes, but remember: those are losses
+    else:
+        raise TypeError("Expected r to be Series or DataFrame")
+
+def var_gaussian(r, level=5, modified=False):
+    """
+    Returns the Parametric Gaussian VaR of a Series or DataFrame
+    If "modified" is True, then the modified VaR is returned,
+    using the Cornish-Fisher modification
+    """
+    # compute the Z score assuming it was Gaussian
+    z = scipy.stats.norm.ppf(level/100)
+    if modified:
+        # modify the Z score based on observed skewness and kurtosis
+        s = skewness(r)
+        k = kurtosis(r)
+        z = (z +
+                (z**2 - 1)*s/6 +
+                (z**3 -3*z)*(k-3)/24 -
+                (2*z**3 - 5*z)*(s**2)/36
+            )
+    return -(r.mean() + z*r.std(ddof=0))
+
+
+def cvar_historic(r, level=5):
+    """
+    Computes the Conditional VaR of Series or DataFrame
+    """
+
+    if isinstance(r, pd.Series):
+        is_beyond = r <= -var_historic(r, level=level)
+        return -r[is_beyond].mean() # average losses AFTER level percentile
+    elif isinstance(r, pd.DataFrame):
+        return r.aggregate(cvar_historic, level=level)  # calls the function again, but this time over each column
+    else:
+        raise TypeError("Expected r to be Series or DataFrame")
